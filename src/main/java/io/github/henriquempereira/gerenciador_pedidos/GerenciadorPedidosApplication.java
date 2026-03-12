e036d637f7c28fba1f2f6001412b7ca2917d0dec
Henrique Morey Pereira <henriquemoreypereira@gmail.com>
2026-03-11 21:57:02 -0300

    feat: Adiciona Repository das entidades e salva os dados no banco de dados

--- src/main/java/io/github/henriquempereira/gerenciador_pedidos/GerenciadorPedidosApplication.java
+++ src/main/java/io/github/henriquempereira/gerenciador_pedidos/GerenciadorPedidosApplication.java
@@ -1,8 +1,17 @@
 package io.github.henriquempereira.gerenciador_pedidos;
 
+import io.github.henriquempereira.gerenciador_pedidos.model.Categoria;
+import io.github.henriquempereira.gerenciador_pedidos.model.Pedido;
+import io.github.henriquempereira.gerenciador_pedidos.model.Produto;
+import io.github.henriquempereira.gerenciador_pedidos.repository.CategoriaRepository;
+import io.github.henriquempereira.gerenciador_pedidos.repository.PedidoRepository;
+import io.github.henriquempereira.gerenciador_pedidos.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class GerenciadorPedidosApplication implements CommandLineRunner {
@@ -11,8 +20,26 @@ public class GerenciadorPedidosApplication implements CommandLineRunner {
 		SpringApplication.run(GerenciadorPedidosApplication.class, args);
 	}
 
+	@Autowired
+	ProdutoRepository produtoRepository;
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	@Autowired
+	PedidoRepository pedidoRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World!");
+
+		Produto produto1 = new Produto("Cheese Burger", 28.90);
+		produtoRepository.save(produto1);
+
+		Categoria categoria1 = new Categoria("Lanches");
+		categoriaRepository.save(categoria1);
+
+		LocalDate localDate = LocalDate.of(2026, 03, 11);
+		Pedido pedido1 = new Pedido(localDate);
+		pedidoRepository.save(pedido1);
+
 	}
 }
